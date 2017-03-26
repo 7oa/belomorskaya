82ffeca8f2f3527897b8779997880f4d7a60d8a3
7oa <semenova.7oa@yandex.ru>
2017-03-26 23:50:15 +0300

    index.html

--- app/js/scripts/script.js
+++ app/js/scripts/script.js
@@ -13,8 +13,11 @@ $(document).ready(function() {
 
     // выбор дат
     $('.select-date-input').datepicker({
-        altField: ".label-date-first",
-        altFormat: "DD MM"
+        dateFormat: "d MM",
+        autoClose: true,
+        language: {
+            months: ['Января','Февраля','Марта','Апреля','Мая','Июня','Июля','Августа','Сентября','Октября','Ноября','Декабря']
+        }
     });
 
     // masonry
@@ -23,4 +26,22 @@ $(document).ready(function() {
     //     gutter: 5,
     //     columnWidth: 390,
     // });
-});
+
+    //кол-во гостей
+    $(".js-plus").click(function () {
+        var count_el = $(this).parent().children(".js-count");
+        var count_input = $(this).parent().children(".js-count-input");
+        var count_num = parseInt(count_el.text());
+        count_el.html(count_num+1);
+        count_input.val(count_num+1);
+    });
+    $(".js-minus").click(function () {
+        var count_el = $(this).parent().children(".js-count");
+        var count_input = $(this).parent().children(".js-count-input");
+        var count_num = parseInt(count_el.text());
+        if(count_num>1) {
+            count_el.html(count_num-1);
+            count_input.val(count_num-1);
+        }
+    });
+})
