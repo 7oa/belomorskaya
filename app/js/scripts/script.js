2846783ad2056aabe08ec458f8933d91b5b4ee2e
7oa <semenova.7oa@yandex.ru>
2017-03-31 12:40:44 +0300

    new

--- app/js/scripts/script.js
+++ app/js/scripts/script.js
@@ -126,4 +126,22 @@ $(document).ready(function() {
         $(".content").toggleClass("menu-overlay");
         $(".header-menu-wrapper").slideToggle();
     })
+
+    //left menu
+	function windowSize(){
+		if ($(window).width() < '1240'){
+		    if($(".active-link").length==0){
+				$(".minittl").after('<div class="active-link"><ul class="left-menu"></ul></div>');
+				$(".left-sidebar .left-menu__link.active").parent().clone().appendTo(".active-link .left-menu");
+            }
+            if($(".noactive-link").length==0){
+				$(".info-slider").after('<div class="noactive-link"><ul class="left-menu"></ul></div>');
+				$(".left-sidebar .left-menu__link:not(.active)").parent().clone().appendTo(".noactive-link .left-menu");
+            }
+		} else {
+			$(".active-link").remove();
+			$(".noactive-link").remove();
+		}
+	}
+	$(window).on('load resize',windowSize);
 });
